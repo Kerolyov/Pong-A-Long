6a2aa8c37ead8b48879c27430b6febec60c75b72
Jon Moore <jonr.moore75@gmail.com>
2016-02-11 13:27:42 +0000

    Window Creation moved to own function
    The Window creation code is separated into its own function

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,6 +2,34 @@
 #include <SDL.h>
 
 SDL_Window* g_pWindow = nullptr;
+int g_WindowID = 0;
+int g_Width = 0;
+int g_Height = 0;
+
+void WindowRelease()
+{
+	// Destroy the window
+	SDL_DestroyWindow(g_pWindow);
+	g_pWindow = nullptr;
+}
+
+bool WindowCreate(std::string title, int x, int y, int w, int h, Uint32 flags)
+{
+	WindowRelease();
+
+	g_pWindow = SDL_CreateWindow(title.c_str(), x, y, w, h, flags);
+
+	if (g_pWindow == nullptr)
+		return false;
+
+	g_Width = w;
+	g_Height = h;
+
+	//Grab window identifier 
+	g_WindowID = SDL_GetWindowID(g_pWindow);
+
+	return true;
+}
 
 bool Init()
 {
@@ -14,17 +42,16 @@ bool Init()
 		return false;
 	}
 
-	// Create a window
-	g_pWindow = SDL_CreateWindow("Test Window",
+	// Create a window, report error if window not created
+	if (!WindowCreate("Test Window",
 		SDL_WINDOWPOS_UNDEFINED,
 		SDL_WINDOWPOS_UNDEFINED,
 		640, 480,
-		SDL_WINDOW_SHOWN);
-
-	// Report error if window not created
-	if (g_pWindow == nullptr)
+		SDL_WINDOW_SHOWN))
 	{
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", "Window Creation Failed", nullptr);
+		std::string err_msg = "Window Creation Failed.\n";
+		err_msg += SDL_GetError();
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", err_msg.c_str(), nullptr);
 		return false;
 	}
 
@@ -33,21 +60,25 @@ bool Init()
 
 void Cleanup()
 {
-	// Destroy the window
-	SDL_DestroyWindow(g_pWindow);
-	g_pWindow = nullptr;
+	WindowRelease();
 
 	// Shutdown SDL
 	SDL_Quit();
 }
 
+void MainLoop()
+{
+	// Wait for 3 seconds
+	SDL_Delay(3000);
+}
+
 int main(int argc, char *argv[])
 {
+	// Initialise SDL and create window
 	if (!Init())
 		return -1;
 
-	// Wait for 3 seconds
-	SDL_Delay(3000);
+	MainLoop();
 
 	Cleanup();
 
